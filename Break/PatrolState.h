7bc4b3b9107bbcfecc6562105a8feae966d02ba5
Han Wei Dylan <dylanhan99@gmail.com>
2019-02-08 15:15:39 +0800

    buncha enemy stattes

---
+++ Break/PatrolState.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "EnemyState.h"
+class PatrolState :
+	public EnemyState
+{
+public:
+	PatrolState();
+	~PatrolState();
+};
+
---
+++ Break/SuspiciousState.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "EnemyState.h"
+class SuspiciousState :
+	public EnemyState
+{
+public:
+	SuspiciousState();
+	~SuspiciousState();
+};
+
